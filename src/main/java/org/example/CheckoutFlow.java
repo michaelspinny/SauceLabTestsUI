bde7d5de4ec2770ed61f8b8f4c6feaf6b45310d1
Mykhailo_Portianov <mykhailo.portianov@boemm.eu>
2022-06-29 01:47:54 +0300

    Sauce Lab Tests (part 2)

--- src/main/java/org/example/CheckoutFlow.java
+++ src/main/java/org/example/CheckoutFlow.java
@@ -5,17 +5,13 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.time.Duration;
-
-public class Login {
+public class CheckoutFlow {
     public static void main(String[] args) {
-        logInTest();
+        checkoutFlowTest();
     }
 
-    public static void logInTest() {
+    public static void checkoutFlowTest() {
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
 
@@ -24,18 +20,25 @@ public class Login {
         driver.findElement(By.id("user-name")).sendKeys("standard_user");
         driver.findElement(By.id("password")).sendKeys("secret_sauce");
         driver.findElement(By.id("login-button")).click();
+        driver.findElement(By.id("add-to-cart-sauce-labs-backpack")).click();
+        driver.findElement(By.id("shopping_cart_container")).click();
+        driver.findElement(By.id("checkout")).click();
+        driver.findElement(By.id("first-name")).sendKeys("Test");
+        driver.findElement(By.id("last-name")).sendKeys("User");
+        driver.findElement(By.id("postal-code")).sendKeys("2233");
+        driver.findElement(By.id("continue")).click();
+        driver.findElement(By.id("finish")).click();
 
         String url = driver.getCurrentUrl();
         System.out.println(url);
 
-        WebElement element = new WebDriverWait(driver, Duration.ofSeconds(3))
-                .until(ExpectedConditions.elementToBeClickable(By.id("react-burger-menu-btn")));
-
-        if (url == "https://www.saucedemo.com/inventory.html") {
+        if (url == "https://www.saucedemo.com/checkout-complete.html") {
             System.out.println("Test Passed");
         } else {
             System.out.println("Test Failed");
         }
+
         driver.quit();
+
     }
 }
