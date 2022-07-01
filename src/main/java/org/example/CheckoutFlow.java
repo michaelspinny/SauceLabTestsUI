9f01fc7b67b3ed168cf197461a3400b0417622d1
Mykhailo_Portianov <mykhailo.portianov@boemm.eu>
2022-07-01 14:27:56 +0300

    Sauce Lab Tests (final)

--- src/main/java/org/example/CheckoutFlow.java
+++ src/main/java/org/example/CheckoutFlow.java
@@ -5,8 +5,25 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class CheckoutFlow {
+
+    private static final String baseURL = "https://www.saucedemo.com/";
+    private static final By usernameLocator = By.id("user-name");
+    private static final By passwordLocator = By.id("password");
+    private static final By loginButtonLocator = By.id("login-button");
+    private static final By addToCartBackpackButtonLocator = By.id("add-to-cart-sauce-labs-backpack");
+    private static final By shoppingCartIconLocator = By.id("shopping_cart_container");
+    private static final By checkoutButtonLocator = By.id("checkout");
+    private static final By firstNameLocator = By.id("first-name");
+    private static final By lastNameLocator = By.id("last-name");
+    private static final By postalCodeLocator = By.id("postal-code");
+    private static final By continueButtonLocator = By.id("continue");
+    private static final By finishButtonLocator = By.id("finish");
     public static void main(String[] args) {
         checkoutFlowTest();
     }
@@ -14,25 +31,47 @@ public class CheckoutFlow {
     public static void checkoutFlowTest() {
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
 
-        driver.get("https://www.saucedemo.com/");
+        driver.get(baseURL);
         driver.manage().window().maximize();
-        driver.findElement(By.id("user-name")).sendKeys("standard_user");
-        driver.findElement(By.id("password")).sendKeys("secret_sauce");
-        driver.findElement(By.id("login-button")).click();
-        driver.findElement(By.id("add-to-cart-sauce-labs-backpack")).click();
-        driver.findElement(By.id("shopping_cart_container")).click();
-        driver.findElement(By.id("checkout")).click();
-        driver.findElement(By.id("first-name")).sendKeys("Test");
-        driver.findElement(By.id("last-name")).sendKeys("User");
-        driver.findElement(By.id("postal-code")).sendKeys("2233");
-        driver.findElement(By.id("continue")).click();
-        driver.findElement(By.id("finish")).click();
+
+        wait.until(ExpectedConditions.visibilityOf(driver.findElement(usernameLocator)));
+        driver.findElement(usernameLocator).sendKeys("standard_user");
+
+        wait.until(ExpectedConditions.visibilityOf(driver.findElement(passwordLocator)));
+        driver.findElement(passwordLocator).sendKeys("secret_sauce");
+
+        wait.until(ExpectedConditions.elementToBeClickable(driver.findElement(loginButtonLocator)));
+        driver.findElement(loginButtonLocator).click();
+
+        wait.until(ExpectedConditions.elementToBeClickable(driver.findElement(addToCartBackpackButtonLocator)));
+        driver.findElement(addToCartBackpackButtonLocator).click();
+
+        wait.until(ExpectedConditions.elementToBeClickable(driver.findElement(shoppingCartIconLocator)));
+        driver.findElement(shoppingCartIconLocator).click();
+
+        wait.until(ExpectedConditions.elementToBeClickable(driver.findElement(checkoutButtonLocator)));
+        driver.findElement(checkoutButtonLocator).click();
+
+        wait.until(ExpectedConditions.visibilityOf(driver.findElement(firstNameLocator)));
+        driver.findElement(firstNameLocator).sendKeys("Test");
+
+        wait.until(ExpectedConditions.visibilityOf(driver.findElement(lastNameLocator)));
+        driver.findElement(lastNameLocator).sendKeys("User");
+
+        wait.until(ExpectedConditions.visibilityOf(driver.findElement(postalCodeLocator)));
+        driver.findElement(postalCodeLocator).sendKeys("2233");
+
+        wait.until(ExpectedConditions.elementToBeClickable(driver.findElement(continueButtonLocator)));
+        driver.findElement(continueButtonLocator).click();
+
+        wait.until(ExpectedConditions.elementToBeClickable(driver.findElement(finishButtonLocator)));
+        driver.findElement(finishButtonLocator).click();
 
         String url = driver.getCurrentUrl();
-        System.out.println(url);
 
-        if (url == "https://www.saucedemo.com/checkout-complete.html") {
+        if (url.equals("https://www.saucedemo.com/checkout-complete.html")) {
             System.out.println("Test Passed");
         } else {
             System.out.println("Test Failed");
--- src/main/java/org/example/Login.java
+++ src/main/java/org/example/Login.java
@@ -9,29 +9,45 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+
+
 
 public class Login {
+
+    private static final String baseURL = "https://www.saucedemo.com/";
+    private static final By usernameLocator = By.id("user-name");
+    private static final By passwordLocator = By.id("password");
+    private static final By loginButtonLocator = By.id("login-button");
+
+
     public static void main(String[] args) {
+
         logInTest();
+
     }
 
     public static void logInTest() {
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
 
-        driver.get("https://www.saucedemo.com/");
+        driver.get(baseURL);
         driver.manage().window().maximize();
-        driver.findElement(By.id("user-name")).sendKeys("standard_user");
-        driver.findElement(By.id("password")).sendKeys("secret_sauce");
-        driver.findElement(By.id("login-button")).click();
+
+        wait.until(ExpectedConditions.visibilityOf(driver.findElement(usernameLocator)));
+        driver.findElement(usernameLocator).sendKeys("standard_user");
+
+        wait.until(ExpectedConditions.visibilityOf(driver.findElement(passwordLocator)));
+        driver.findElement(passwordLocator).sendKeys("secret_sauce");
+
+        wait.until(ExpectedConditions.elementToBeClickable(driver.findElement(loginButtonLocator)));
+        driver.findElement(loginButtonLocator).click();
 
         String url = driver.getCurrentUrl();
-        System.out.println(url);
 
-        WebElement element = new WebDriverWait(driver, Duration.ofSeconds(3))
-                .until(ExpectedConditions.elementToBeClickable(By.id("react-burger-menu-btn")));
-
-        if (url == "https://www.saucedemo.com/inventory.html") {
+        if (url.equals("https://www.saucedemo.com/inventory.html")) {
             System.out.println("Test Passed");
         } else {
             System.out.println("Test Failed");
